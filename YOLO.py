1759e3e7baa48476451b7e4e047bea42661b1a1d
Rain Jocas <rainjocas@gmail.com>
2025-04-02 22:37:31 -0400

    working on buses dataset

--- YOLO.py
+++ YOLO.py
@@ -10,55 +10,126 @@
   orcid = {0000-0001-5950-6979}
 }
 """
+import os
 import pandas as pd
 import torch
 import numpy as np
 import torchvision.io as io
-#import selectivesearch
-#import torch_snippets
-#from torch_snippets import *
-#import torch_snippets
 import cv2
-
-"""
-#get class names
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s',  pretrained=True)
-results = model(img)
-classes = results.names
-
-"""
-
+from os import listdir
 import torch
+
+device = 'mps' if torch.backends.mps.is_available() else 'cpu'
 
 # Model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
-# Batch of images
-images = ["Media/Crows.jpg", "Media/Dog Park.jpg",
-          "Media/Petting Zoo.jpg", "Media/Street.jpg",
-          "Media/Cars Movie.png"]
+def runYOLOSmBatch():
+  """ Runs YOLO on a small batch of images, saves the newly bounded images"""
+  # Batch of images
+  images = ["Media/Crows.jpg", "Media/Dog Park.jpg",
+            "Media/Petting Zoo.jpg", "Media/Street.jpg",
+            "Media/Cars Movie.png"]
 
-# Inference
-results = model(images)
+  # Inference
+  results = model(images)
 
-# Results
-results.print()
-results.save()  # or .show()
+  # Save new images with bounding boxes and classes
+  results.save()
 
-results.xyxy[0]  # img1 predictions (tensor)
-results.pandas().xyxy[0]  # img1 predictions (pandas)
-
-print(results)
-
-print("Hello World")
-# results.print()
-# classes = results.names
-# print(classes)
-
-
-#torch_snippets.show(im)
+  # Results
+  results.print()
 
 #Load Busses dataset
 IMAGE_ROOT = 'archive/images'
 df = pd.read_csv('archive/df.csv')
 print("First 5 records:", df.head())
+
+#grab filenames
+fileNames = os.listdir('archive/images/images')
+
+#convert filenames to file paths
+imgs = []
+
+for fileName in fileNames:
+    filePath = 'archive/images/images/' + fileName
+    imgs.append(filePath)
+    #print(img)
+
+print("Images:", imgs[0:5])
+
+#testing on smaller set because it keeps crashing
+imgs = imgs[0:5]
+
+# Inference
+results = model(imgs)
+# results = model(imgs).to(device)
+
+print("ELEPHANT")
+
+# Results
+results.print()
+
+results.xyxy[0]  # img1 predictions (tensor)
+results.pandas().xyxy[0]  # img1 predictions (pandas)
+
+print(results.pandas().xyxy[0])
+
+def getConfidences():
+  """ Returns a list of average confidence for all images """
+  confidences = []
+  for i in range(len(imgs)):
+      #get confidences for an image
+      confidence = results.pandas().xyxy[i].confidence
+      #average all confidences in an image & add to list
+      avgConfidence = sum(confidence)/ len(confidence)
+      confidences.append(avgConfidence)
+  return confidences
+
+def getIoUs():
+  """ Returns a list of average confidence for all images """
+  IoUs = []
+  # for i in range(len(imgs)):
+    #get IoUs for an image
+
+
+      #IoUs.append(avgConfidence)
+  return IoUs
+
+def getDetectionAcc(confidences, IoUs):
+  """ Calculates and returns the detection accuracy for cofidence levels and IoU """
+  confThreshold = 0.8
+  IoUThreshold = 0.8
+
+  #get number of images that pass the confidence threshold
+  totPassedConf = 0
+  for conf in confidences:
+      if conf >= confThreshold:
+          totPassedConf += 1
+
+  #detection accuracy for confidence levels
+  confAcc = totPassedConf/len(confidences)
+
+  #get number of images that pass the IoU threshold
+  totPassedIoU = 0
+  for IoU in IoUs:
+      if IoU >= IoUThreshold:
+          totPassedIoU += 1
+
+  #detection accuracy for IoU
+  IoUAcc = totPassedIoU/len(IoUs)
+
+  return (confAcc, IoUAcc)
+
+confidences = getConfidences()
+IoUs = getIoUs()
+
+confAcc, IoUAcc = getDetectionAcc(confidences, IoUs)
+
+
+"""
+You can establish a threshold value for confidence level and another for IoU that
+considers the detection successful or failed. Then compute the detection accuracy
+based on that. In order to compute the IoU between two bounding boxes, write a
+function compute_IoU that does that.
+"""
